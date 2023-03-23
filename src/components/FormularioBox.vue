9cef7456c039a23ef9422720b6d050a7d0d8e1d2
Vitoriano <vitorianoernandes2012@gmail.com>
2023-03-22 23:08:04 -0300

    Criando o component de cronometro

--- src/components/FormularioBox.vue
+++ src/components/FormularioBox.vue
@@ -16,11 +16,8 @@
         <div
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
+         <Cronometro :tempo-em-segundos="tempoEmSegundos" />
+
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -41,20 +38,20 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Cronometro from './Cronometro.vue';
 
 export default defineComponent({
   name: "FormulárioBox",
+  components: {
+    Cronometro
+  },
   data () {
     return {
       tempoEmSegundos: 0,
       cronometro: 0
     }
   },
-  computed: {
-    tempoDecorrido () : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-    }
-  },
+ 
   methods: {
     iniciar () {
       // começar a contagem
